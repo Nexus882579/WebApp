ca444c5c7160089fd1da036da30dd4b94b5519e6
Nexus882579 <gdn811027@gmail.com>
2025-04-18 01:11:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 const tg = Telegram.WebApp;
 tg.ready()
 tg.expand()
-
-
-var welcome = document.getElementById('welcome')
-
-welcome.textContent = `Добро пожаловать ${tg.initDataUnsafe.user.first_name}!`
 tg.MainButton.text = "Главная кнопка"
 tg.MainButton.show()
